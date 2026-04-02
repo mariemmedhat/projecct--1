3f87bf36da5a37e24f6ca9f7a9c834fb6dc04a65
mariemmedhat <medhatmariem35@gmail.com>
2026-04-02 15:31:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,62 +15,62 @@ let menuItems = [
 name: "LASAL CHEESE",
 price: "$18.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food1.png"
+image: "food1.png"
 },{
 name: "JUMBO CRAB SHRIMP",
 price: "$24.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food2.png"
+image: "food2.png"
 }, {
 name: "KOKTAIL JUICE",
 price: "$12.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food3.png"
+image: "food3.png"
 }, {
 name: "CAPO STEAK",
 price: "$60.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food4.png"
+image: "food4.png"
 },{
 name: "ORGANIC FRUIT SALAD",
 price: "$8.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food5.png"
+image: "food5.png"
 },{
 name: "CHEESE PIZZA",
 price: "$18.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food6.png"
+image: "food6.png"
 },{
 name: "KOFTA MEAT",
 price: "$40.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food7.jpeg"
+image: "food7.jpeg"
 },{
 name: "SPANISH PIES",
 price: "$14.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food8.jpeg"
+image: "food8.jpeg"
 }, {
 name: "CHEESE TOST",
 price: "$6.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food9.jpeg"
+image: "food9.jpeg"
 },{
 name: "FRUIT SALAD",
 price: "$14.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food10.jpeg"
+image: "food10.jpeg"
 },{
 name: "CHICKEN SHAWARMA",
 price: "$20.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food11.jpeg"
+image: "food11.jpeg"
 },{
 name: "MEGA CHEESE PIZZA",
 price: "$30.00",
 description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.At, explicabo.",
-image: "images/food12.jpeg"
+image: "food12.jpeg"
 }
 ]
 let cart=""
